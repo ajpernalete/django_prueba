5ef970fce8ab7d134197b49a9276084fd9da3da9
Alberto <ajpernaletel@gmail.com>
2020-04-14 03:23:06 -0400

    Se bloquea accesos por url dependiendo de si un usuario esta registrado o no. Se cambio icono enlace Registro

--- pernalete_store/views.py
+++ pernalete_store/views.py
@@ -24,6 +24,9 @@ def index(request):
     })
 
 def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+        
     if request.method == 'POST':
         username = request.POST.get('username') 
         password = request.POST.get('password')
@@ -47,6 +50,9 @@ def logout_view(request):
     return redirect('login')
 
 def register(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+
     form = RegisterForm(request.POST or None)
 
     if request.method == 'POST' and form.is_valid():
