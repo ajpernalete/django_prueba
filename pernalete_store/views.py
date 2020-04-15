b2fbc14289dcc3eb37acdd118f28eec9920ba239
Alberto <ajpernaletel@gmail.com>
2020-04-15 17:19:29 -0400

    Se reemplazo los productos actuales por los de la app products y se quito condición de stock de inventario por mostrar precio. Luego se ordeno la lista de registro de forma inversa para mostrar el mas reciente primero

--- pernalete_store/views.py
+++ pernalete_store/views.py
@@ -11,16 +11,17 @@ from django.contrib.auth.models import User
 
 from .forms import RegisterForm
 
+from products.models import Product
+
 def index(request):
+
+    product = Product.objects.all().order_by('-id')
+
+
     return render(request,'index.html', {
         'message':'Lisado de productos',
         'title':'Productos',
-        'products':[
-            {'title':'Franela','price':5,'stock':True},#producto
-            {'title':'Camisa','price':7,'stock':True},
-            {'title':'Mochila','price':20,'stock':False},
-            {'title':'Laptop','price':500,'stock':True}
-        ]
+        'products':product
     })
 
 def login_view(request):
