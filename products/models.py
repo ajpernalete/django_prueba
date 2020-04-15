f11c51eaee1b105456f33474ad7779cd1eb506ab
Alberto <ajpernaletel@gmail.com>
2020-04-15 19:10:44 -0400

    Se colocaron slug automaticos con la libreria uuid con el metodo uuid4. Se colocaron enlaces a los productos en el index.html

--- products/models.py
+++ products/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from django.utils.text import slugify
 
@@ -19,6 +21,15 @@ class Product(models.Model):
       return self.title
 
 def set_slug(sender,instance, *args, **kwargs): #callback
-   instance.slug = slugify(instance.title)
+   if instance.title and not instance.slug:
+      slug = slugify(instance.title)
+
+      while Product.objects.filter(slug=slug).exists():
+         slug = slugify(
+            '{}-{}'.format(instance.title, str(uuid.uuid4())[:8] )
+         )
+
+      instance.slug = slug
+   
 
 pre_save.connect(set_slug, sender=Product)
