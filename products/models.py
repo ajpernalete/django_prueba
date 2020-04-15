a06fec4df12b43dede27ec9753cf3a0e07a88351
Alberto <ajpernaletel@gmail.com>
2020-04-15 18:55:45 -0400

    Se creo slug automaticos con callbacks y Signals con metodo pre_save()

--- products/models.py
+++ products/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.utils.text import slugify
 
+from django.db.models.signals import pre_save
+
+
 class Product(models.Model):
    title = models.CharField(max_length=50)
    description = models.TextField()
@@ -8,9 +11,14 @@ class Product(models.Model):
    slug = models.SlugField(null=False,blank=False,unique=True )
    created_at = models.DateTimeField(auto_now_add=True)
 
-   def save(self, *args, **kwargs):
+   """def save(self, *args, **kwargs):
       self.slug = slugify(self.title)
-      super(Product, self).save(*args, **kwargs)
+      super(Product, self).save(*args, **kwargs)"""
 
    def __str__(self):
       return self.title
+
+def set_slug(sender,instance, *args, **kwargs): #callback
+   instance.slug = slugify(instance.title)
+
+pre_save.connect(set_slug, sender=Product)
