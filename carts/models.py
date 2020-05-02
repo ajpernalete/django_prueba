19a348b3077c08eb926886e1bb2a5327acc928e7
Alberto <ajpernaletel@gmail.com>
2020-05-02 19:24:16 -0400

    Se calcula subtotal y total de productos del carrito. Se agrego en el carrito el subtotal.

--- carts/models.py
+++ carts/models.py
@@ -1,4 +1,5 @@
 import uuid
+import decimal
 
 from django.db import models
 
@@ -6,6 +7,7 @@ from users.models import User
 from products.models import Product
 
 from django.db.models.signals import pre_save
+from django.db.models.signals import m2m_changed
 
 class Cart(models.Model):
     cart_id = models.CharField(max_length=100,null=False,blank=False,unique=True)
@@ -15,11 +17,30 @@ class Cart(models.Model):
     total = models.DecimalField(default=0.0, max_digits=8,decimal_places=2)
     creates_at = models.DateTimeField(auto_now_add=True)
 
+    FEE = 0.05 # 0.5%
+
     def __str__(self):
         return self.cart_id
+
+    def update_totals(self):
+        self.update_subtotal()
+        self.update_total()
+
+    def update_subtotal(self):
+        self.subtotal = sum([ product.price for product in self.products.all() ])
+        self.save()
+
+    def update_total(self):
+        self.total = self.subtotal + (self.subtotal * decimal.Decimal(Cart.FEE) )
+        self.save()
 
 def set_cart_id(sender, instance, *args, **kwargs):
     if not instance.cart_id:
         instance.cart_id = str(uuid.uuid4())
 
+def update_totals(sender, instance, action, *args, **kwargs):
+    if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
+        instance.update_totals()
+
 pre_save.connect(set_cart_id, sender = Cart)
+m2m_changed.connect(update_totals, sender=Cart.products.through)
