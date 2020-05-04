ccf6f1b34d4a6bf6fe94cabe35383f1528ef6994
Alberto <ajpernaletel@gmail.com>
2020-05-04 00:23:36 -0400

    Se pueden añadir varios items por productos. Se actualizo calculo de total y subtotal, asi como el mensaje de producto agregado correctamente en singular y/o plural. Se cambio la estructura de la base de datos.

--- carts/models.py
+++ carts/models.py
@@ -7,12 +7,13 @@ from users.models import User
 from products.models import Product
 
 from django.db.models.signals import pre_save
+from django.db.models.signals import post_save
 from django.db.models.signals import m2m_changed
 
 class Cart(models.Model):
     cart_id = models.CharField(max_length=100,null=False,blank=False,unique=True)
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE) #Uno a muchos
-    products = models.ManyToManyField(Product) #Muchos a muchos
+    products = models.ManyToManyField(Product, through='CartProducts') #Muchos a muchos
     subtotal = models.DecimalField(default=0.0, max_digits=8,decimal_places=2)
     total = models.DecimalField(default=0.0, max_digits=8,decimal_places=2)
     creates_at = models.DateTimeField(auto_now_add=True)
@@ -27,11 +28,39 @@ class Cart(models.Model):
         self.update_total()
 
     def update_subtotal(self):
-        self.subtotal = sum([ product.price for product in self.products.all() ])
+        self.subtotal = sum([ 
+            cp.quantity * cp.product.price for cp in self.products_related()
+         ])
         self.save()
 
     def update_total(self):
         self.total = self.subtotal + (self.subtotal * decimal.Decimal(Cart.FEE) )
+        self.save()
+
+    def products_related(self):
+        return self.cartproducts_set.select_related('product')
+
+class CartProductsManager(models.Manager):
+
+    def create_or_update_quantity(self, cart, product, quantity = 1):
+        object, created = self.get_or_create(cart=cart,product=product)
+
+        if not created:
+            quantity = object.quantity + quantity
+
+        object.update_quantity(quantity)
+        return object
+
+class CartProducts(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    objects = CartProductsManager()
+
+    def update_quantity(self, quantity = 1):
+        self.quantity = quantity
         self.save()
 
 def set_cart_id(sender, instance, *args, **kwargs):
@@ -42,5 +71,9 @@ def update_totals(sender, instance, action, *args, **kwargs):
     if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
         instance.update_totals()
 
+def post_save_update_totals(sender, instance, *args, **kwargs):
+    instance.cart.update_totals()
+
 pre_save.connect(set_cart_id, sender = Cart)
+post_save.connect(post_save_update_totals, sender= CartProducts)
 m2m_changed.connect(update_totals, sender=Cart.products.through)
--- carts/views.py
+++ carts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 
 from products.models import Product
 
+from .models import CartProducts
 from .models import Cart
 from.utils import get_or_create_cart 
 
@@ -15,10 +16,19 @@ def cart(request):
 def add(request):
     cart = get_or_create_cart(request)
     product = get_object_or_404(Product, pk=request.POST.get('product_id'))
+    quantity = int(request.POST.get('quantity', 1))
 
-    cart.products.add(product)
+    '''cart.products.add(product, through_defaults={
+        'quantity': quantity
+    })'''
+    cart_product = CartProducts.objects.create_or_update_quantity(cart=cart, 
+                                                product=product,
+                                                quantity=quantity)
+
 
     return render(request, 'carts/add.html', {
+        'quantity': quantity,
+        'cart_product': cart_product,
         'product': product
     })
 
